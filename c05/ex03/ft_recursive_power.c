d0e2621ca470c626d37812ed1ddd270f7b65bb26
Sami El Mallem <sel-mall@shi-r5-p6.s19.be>
2025-02-26 17:40:15 +0100

    a ajouter c05

---
+++ c05/ex03/ft_recursive_power.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sel-mall <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/26 16:25:36 by sel-mall          #+#    #+#             */
+/*   Updated: 2025/02/26 16:25:38 by sel-mall         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_recursive_power(int nb, int power)
+{
+	int	i;
+
+	i = nb;
+	if (power < 0)
+		return (0);
+	else if (power == 0)
+		return (1);
+	else
+		return (nb * ft_recursive_power(nb, power - 1));
+}
